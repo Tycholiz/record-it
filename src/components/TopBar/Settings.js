15854a812a06ea48cfe15945d8d090686dbe6226
Kyle Tycholiz <tycholiz22@hotmail.com>
2018-11-08 17:26:48 -0800

    blank modal will show up on elipsis press

--- src/components/TopBar/Settings.js
+++ src/components/TopBar/Settings.js
@@ -5,11 +5,13 @@ import PropTypes from 'prop-types';
 import {
 	StyleSheet,
 	TouchableOpacity,
-	Modal
+	Modal,
+	Text,
+	View,
+	TouchableHighlight
 } from 'react-native';
 
 import { toggleOptions } from '../../actions'
-
 
 import Icon from 'react-native-vector-icons/FontAwesome';
 const optionsIcon = (<Icon name="ellipsis-v" size={40} color='black' />)
@@ -20,15 +22,32 @@ class Settings extends Component {
 		const { dispatch, optionsOpen } = this.props;
 		dispatch(toggleOptions());
 
-		//change state to settingsOpen: true
 		//below in render() section, render conditionally the modal if settingsOpen = true
 		console.log("hey mama")
 	}
 
 	render() {
+		const { optionsOpen } = this.props;
 		return (
 			<TouchableOpacity style={styles.settingsIcon} onPress={() => this.handleOpenSettings()}>
 				{optionsIcon}
+
+				<Modal
+					animationType="fade"
+					transparent={false}
+					visible={optionsOpen}
+					onRequestClose={() => { () => this.handleOpenSettings()}}>
+					<View style={styles.modal}>
+						<View style={styles.container}>
+							<Text>Hello World!</Text>
+
+							<TouchableHighlight onPress={() => this.handleOpenSettings()}>
+								<Text>Hide Modal</Text>
+							</TouchableHighlight>
+						</View>
+					</View>
+				</Modal>
+
 			</TouchableOpacity>
 		);
 	}
@@ -48,5 +67,19 @@ const styles = StyleSheet.create({
 		height: 60,
 		justifyContent: 'center',
 		alignItems: 'center',
+	},
+	modal: {
+		// marginTop: 22,
+		flex: 1,
+		backgroundColor: 'rgba(0, 0, 0, 0.5)',
+
+	},
+	container: {
+		flex: 1,
+		margin: 22,
+		backgroundColor: 'white',
+	},
+	closeModalButton: {
+		paddingBottom: 10,
 	},
 });
