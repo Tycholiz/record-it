be62f5125755e5c87b22bf245f090d4a60563c7b
Kyle Tycholiz <tycholiz22@hotmail.com>
2018-12-07 12:44:49 -0800

    delete unit from playback control works

--- src/components/Control/PlaybackControl.js
+++ src/components/Control/PlaybackControl.js
@@ -6,23 +6,61 @@ import {
 	Text,
 	TouchableOpacity,
 	Image,
+	KeyboardAvoidingView,
+	TouchableHighlight
 } from 'react-native';
 import s from '../../styles/Control/PlaybackControl';
+import Modal from "react-native-modal";
 
-import { startPlaying } from '../../actions/index'
+import { UnitType } from '../../constants/enumerables';
+
+import { startPlaying, deleteUnit, setActiveFile } from '../../actions/index'
 
 class PlaybackControl extends Component {
+	state = {
+		deleteConfirmation: false,
+	};
+
+	handleOpenModal = () => {
+		this.setState(() => {
+			return {
+				deleteConfirmation: true
+			};
+		});
+	};
+
+	handleCloseModal = () => {
+		this.setState(() => {
+			return {
+				deleteConfirmation: false
+			};
+		});
+	}
+
+	handleDelete = (unitId) => {
+		const { dispatch } = this.props;
+
+		dispatch(setActiveFile(null));
+		dispatch(deleteUnit(unitId, UnitType.File));
+		this.handleCloseModal();
+	};
+
+	handlePlayButton = () => {
+		const { dispatch } = this.props;
+		dispatch(startPlaying());
+	};
+
 	render() {
-		const { startPlaying, playing, activeFile, title } = this.props;
+		const { playing, activeFile, title } = this.props;
 		return (
 			<View style={s.container}>
-				{/* {activeFile ? ( */}
+				{activeFile ? (
 
 					<View>
 						<View style={s.topLine}>
 							<Text style={s.text}>{title}.mp3</Text>
 							<View style={s.iconContainer}>
-								<TouchableOpacity>
+								<TouchableOpacity onPress={() => this.handleOpenModal()}>
 									<Image source={require('../../../assets/images/garbage.png')} style={{ width: 20, height: 25, margin: 6 }} />
 								</TouchableOpacity>
 							</View>
@@ -44,7 +82,7 @@ class PlaybackControl extends Component {
 							<TouchableOpacity>
 								<Image source={require('../../../assets/images/fastbackward.png')} style={{ width: 55, height: 40 }} />
 							</TouchableOpacity>
-							<TouchableOpacity onPress={startPlaying}>
+							<TouchableOpacity onPress={() => this.handlePlayButton()}>
 								{playing ?
 									<Image source={require('../../../assets/images/play.png')} style={{ width: 70, height: 70 }} />
 										:
@@ -57,11 +95,48 @@ class PlaybackControl extends Component {
 						</View>
 
 					</View>
-				{/* )	: (
+					)	: (
 					<View>
 						<Text>Please select an audio clip</Text>
 					</View>
-				)} */}
+					)
+				}
+
+				{/* DELETE CONFIRMATION MODAL */}
+					<Modal
+						animationType="slide"
+						transparent={true}
+						visible={this.state.deleteConfirmation}
+						onRequestClose={() => {
+							Alert.alert('Modal has been closed.');
+						}}
+					>
+						<KeyboardAvoidingView style={s.modalMask} behavior="padding">
+							<View style={s.modalContainer}>
+								<Text>Are you sure you want to delete {title}?</Text>
+								<View style={s.modalOptions}>
+									<TouchableHighlight
+										onPress={() => {
+											this.handleCloseModal();
+										}}
+										style={s.modalOption}
+									>
+										<Text>CANCEL</Text>
+									</TouchableHighlight>
+
+									<TouchableHighlight
+										onPress={() => {
+											this.handleDelete(activeFile)
+										}}
+										style={[s.modalOption, s.renameOption]}
+									>
+										<Text style={{ color: 'white' }}>CONFIRM</Text>
+									</TouchableHighlight>
+								</View>
+								<Text>This action is not reversible</Text>
+							</View>
+						</KeyboardAvoidingView>
+					</Modal>
 				</View>
 		);
 	}
@@ -71,8 +146,8 @@ mapStateToProps = (state) => {
 	return {
 		playing: state.toggle.playing,
 		activeFile: state.activeFile,
-		title: state.units.files[state.activeFile].title,
+		title: state.activeFile && state.units.files[state.activeFile].title,
 	};
 };
 
-export default connect(mapStateToProps, { startPlaying })(PlaybackControl);
+export default connect(mapStateToProps)(PlaybackControl);
