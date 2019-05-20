21c5206e8abb607a6f58aca4782f19ace96f2e16
Kyle Tycholiz <tycholiz22@hotmail.com>
2019-05-19 21:32:15 -0700

    convert menuoptions component into a functional component

--- src/components/FolderStructure/Menu.js
+++ src/components/FolderStructure/Menu.js
@@ -12,41 +12,41 @@ import { UnitType } from '../../constants/enumerables';
 
 import Menu, { MenuItem, MenuDivider } from 'react-native-material-menu';
 
-class MenuOptions extends Component {
-	render() {
-		const { unitType, handleOpenModal, showMenu, setMenuRef, hideMenu } = this.props;
-
-		return (
-			<TouchableOpacity
-				style={s.folderOptionsContainer}
-				onPress={showMenu}
-			>
-				<Menu
-					ref={setMenuRef}
-					button={<Image source={require('../../../assets/images/settings.png')} style={{ height: 0, width: 0 }} />}
-				>
-					<MenuItem onPress={() => handleMoveUnit(id)}>Move</MenuItem>
+const MenuOptions = ({
+	unitType,
+	handleOpenModal,
+	showMenu,
+	setMenuRef,
+	hideMenu
+}) => (
+	<TouchableOpacity
+		style={s.folderOptionsContainer}
+		onPress={showMenu}
+	>
+		<Menu
+			ref={setMenuRef}
+			button={<Image source={require('../../../assets/images/settings.png')} style={{ height: 0, width: 0 }} />}
+		>
+			<MenuItem onPress={() => handleMoveUnit(id)}>Move</MenuItem>
+			<MenuDivider />
+			<MenuItem onPress={() => handleOpenModal('renameModal')}>Rename</MenuItem>
+			<MenuDivider />
+			<MenuItem onPress={() => handleOpenModal('deleteModal')}>Delete</MenuItem>
+			<MenuDivider />
+			<MenuItem onPress={hideMenu}>Favorite</MenuItem>
+			{unitType === UnitType.File &&
+				<View>
 					<MenuDivider />
-					<MenuItem onPress={() => handleOpenModal('renameModal')}>Rename</MenuItem>
-					<MenuDivider />
-					<MenuItem onPress={() => handleOpenModal('deleteModal')}>Delete</MenuItem>
-					<MenuDivider />
-					<MenuItem onPress={hideMenu}>Favorite</MenuItem>
-					{unitType === UnitType.File &&
-						<View>
-							<MenuDivider />
-							<MenuItem onPress={hideMenu}>Share</MenuItem>
-						</View>
-					}
-					<MenuDivider />
-					<MenuItem onPress={() => handleOpenModal('moreInfoModal')}>More info...</MenuItem>
-					<MenuDivider />
-					<MenuItem onPress={hideMenu}>Close</MenuItem>
-				</Menu>
-			</TouchableOpacity>
-		)
-	}
-}
+					<MenuItem onPress={hideMenu}>Share</MenuItem>
+				</View>
+			}
+			<MenuDivider />
+			<MenuItem onPress={() => handleOpenModal('moreInfoModal')}>More info...</MenuItem>
+			<MenuDivider />
+			<MenuItem onPress={hideMenu}>Close</MenuItem>
+		</Menu>
+	</TouchableOpacity>
+)
 
 MenuOptions.propTypes = {
 	unitType: T.string.isRequired,
